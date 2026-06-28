d71ed2d25d340b2673beff283a788fa9a1688972
Ecl4ts <233498003+Ecl4ts@users.noreply.github.com>
2026-06-29 00:07:49 +0100

    Refactor role handling and add permission checks
    Refactor role handling to use environment variables for booster and member roles. Add permission checks for purge and wipe commands.

--- script.py
+++ script.py
@@ -8,6 +8,9 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='.', intents=intents)
 
+booster_role = os.getenv('BOOSTER_ROLE')
+member_role  = os.getenv('MEMBER_ROLE')
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
@@ -18,14 +21,15 @@ async def on_message(message):
         return
     await bot.process_commands(message)
 
+
 @bot.event
 async def on_member_join(member):
-    role = member.guild.get_role(1520444030855676004)
-    
+    role = member.guild.get_role(member_role)
+    await member.add_roles(role)
 
 @bot.command()
 async def check_booster(ctx, member: discord.Member):
-    role = member.guild.get_role(1520440841179631716)
+    role = member.guild.get_role(booster_role)
     if member.premium_since:
         await ctx.send(f"{member} is boosting!")
         await member.add_roles(role)
@@ -37,7 +41,6 @@ async def delete_webhook(webhook):
     async with aiohttp.ClientSession() as session:
         async with session.delete(webhook) as response:
             if response.status == 204:
-                print("done")
                 return "Deleted"
             else:
                 data = await response.json()
@@ -49,11 +52,13 @@ async def delete(ctx, webhook):
     await ctx.message.reply(message)
 
 @bot.command()
+@commands.has_permissions(manage_messages=True)
 async def purge(ctx, amount: int):
     await ctx.channel.purge(limit=(amount + 1))
     await ctx.send(f"Purged {amount} messages", delete_after=5)
 
 @bot.command()
+@commands.has_permissions(manage_channels=True)
 async def wipe(ctx):
     await ctx.channel.purge()
     await ctx.send(f"Wiped channel", delete_after=5)
